1846360973705d16ea7eb332b7adcd6a9b8cd244
chrhfoq123 <chrhfoq123@naver.com>
2023-09-10 18:21:54 +0900

    jpa 특정 필드로 정보 조회

--- src/main/java/com/example/member/service/MemberService.java
+++ src/main/java/com/example/member/service/MemberService.java
@@ -6,6 +6,8 @@ import com.example.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -17,5 +19,27 @@ public class MemberService {
         // 2. repository의 save 메서드 호출
         memberRepository.save(memberEntity);
         //repository의 save메서드 호출(entity객체를 넘겨줘야 함)
+    }
+
+    public MemberDTO login(MemberDTO memberDTO){
+        /*
+            1. 회원이 입력한 이메일로 DB를 조회함
+            2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치한지 확인
+        */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if(byMemberEmail.isPresent()){
+            //조회 결과가 있음
+            MemberEntity memberEntity = byMemberEmail.get();
+            if(memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())){
+                //entity -> dto 변환 후 리턴
+                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+                return dto;
+            }else{
+                return null;
+            }
+        }else{
+            //조회 결과가 없음
+            return null;
+        }
     }
 }
