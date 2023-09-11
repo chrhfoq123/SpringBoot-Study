066734833ee158c546c6e59ed4b100c10d5fde08
chrhfoq123 <chrhfoq123@naver.com>
2023-09-11 20:48:02 +0900

    session 로그아웃 기능

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -93,4 +93,12 @@ public class MemberController {
 
         return "redirect:/member/";
     }
+
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+
+        return "index";
+    }
+
 }
